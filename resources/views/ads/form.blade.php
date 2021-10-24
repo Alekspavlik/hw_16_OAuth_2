1371fdd1ace4f0d6357e354be66dd643f0bbdecd
oleksandrmorozov <morozovoleksandr86@gmail.com>
2021-10-24 09:37:17 +0300

    hw_15.

--- resources/views/ads/form.blade.php
+++ resources/views/ads/form.blade.php
@@ -6,10 +6,16 @@
         <div class="mb-3">
             <label for="title" class="form-label">Title</label>
             <input type="text" name="title" class="form-control" id="title" value="{{ old('title', $ads->title ?? '') }}">
+            @if($errors->has('title'))
+                <div class="alert alert-danger">{{ $errors->first('title') }}</div>
+            @endif
         </div>
         <div class="mb-3">
             <label for="description" class="form-label">Description</label>
             <textarea name="description" class="form-control" id="description" rows="3">{{ old('description', $ads->description ?? '') }}</textarea>
+            @if($errors->has('description'))
+                <div class="alert alert-danger">{{ $errors->first('description') }}</div>
+            @endif
         </div>
             <input type="submit" class="btn btn-primary" value="@if(!isset($ads->id))Create @else Save @endif"/>
     </form>
--- resources/views/ads/show.blade.php
+++ resources/views/ads/show.blade.php
@@ -2,6 +2,12 @@
 
 @section('content')
             <h2>{{ $ad->title }}</h2>
+            @can('update', $ad)
+                <ul>
+                    <li><a href="{{ route('ads.create', $ad->id) }}">Edit</a></li>
+                    <li><a href="{{ route('ads.delete', $ad->id) }}">Delete</a></li>
+                </ul>
+            @endcan
             <h5>{{ $ad->user->name }}</h5>
             <h5>{{ $ad->created_at->diffForHumans() }}</h5>
             <p>{{ $ad->description }}</p>
